2025c6a3ca86132bb4947ffc4bdcad68058a2bef
Iván Trapero <regenzel@gmail.com>
2025-12-14 00:27:29 +0100

    Añado control de errores y mejoras al programa.

--- Reto1.py
+++ Reto1.py
@@ -1,37 +1,54 @@
-students = {"Manolo":6, "Javier": 4, "Alfonso": 7}
-student_name = ""
-student_grade = 0
 # Tiene que: AGREGAR estudiantes, QUITAR estudiantes.
 # MOSTRAR todos los estudiantes aprobados, BUSCAR estudiantes por nombre y mostrar su promedio.
+# mostrar TODOS los estudiantes.
 # SALIR del programa.
-def average_grade(grade1, grade2, grade3):
-    return (grade1 + grade2 + grade3) / 3
+students = {}
+student_name = ""
+student_grade = 0
+continue_program = True
+key_words = ["agregar", "quitar", "mostrar", "buscar", "todos", "salir"]
 
-question = input("¿Agregar estudiantes, quitar estudiantes, mostrar los aprobados o buscar el promedio de uno?" \
-"(Responda agregar/quitar/mostrar/buscar/salir)")
-# AGREGA a 'students' nuevos estudiantes con sus promedios de notas.
-if question == "agregar":
-    student_name = input("¿Como se llama el estudiante?: ")
-    student_grade1 = input("Ingrese la primera nota: ")
-    student_grade2 = input("Ingrese la segunda nota: ")
-    student_grade3 = input("Ingrese la tercera nota: ")
-    avg = average_grade(float(student_grade1),float(student_grade2),float(student_grade3))
-    students[student_name] = avg
-    print(students)
+def average(num1, num2, num3):
+    return (num1 + num2 + num3) / 3
+        
 
-# QUITA de 'students' al estudiante que escriba el usuario. 
-if question == "quitar":
-    student_name = input("¿Como se llama el estudiante?: ")
-    students.pop(student_name)
-    print(students)
-# MUESTRA los estudiantes aprobados.
-if question == "mostrar":
-    for key,value in students.items():
-        if value >= 6:
-            print(f"{key}, nota media de: {value}")
-# BUSCA al estudiante que el usuario escriba y devuelve su nombre con su respectivo promedio.
-if question == "buscar":
-    student_name = input("¿Como se llama el estudiante?: ")
-    print(f"{student_name}, nota media de: {students.get(student_name)}")
-if question == "salir":
-    pass
+while continue_program == True:
+    question = input("¿Agregar estudiantes, quitar estudiantes, mostrar los aprobados, mostrar todos o buscar el promedio de uno?" \
+    "(Responda agregar/quitar/mostrar/buscar/todos/salir): ")
+    # Si la respuesta esta dentro de lo esperado, se ejecuta
+    if question in key_words: 
+    # AGREGA a 'students' nuevos estudiantes con sus promedios de notas.
+        if question == "agregar":
+            student_name = input("¿Como se llama el estudiante?: ")
+            student_grade1 = input("Ingrese la primera nota: ")
+            student_grade2 = input("Ingrese la segunda nota: ")
+            student_grade3 = input("Ingrese la tercera nota: ")
+            avg = average(float(student_grade1),float(student_grade2),float(student_grade3))
+            students[student_name] = avg
+            print(f'Has añadido a "{student_name}" con promedio de nota de {avg}')
+
+        # QUITA de 'students' al estudiante que escriba el usuario. 
+        if question == "quitar":
+            student_name = input("¿Como se llama el estudiante?: ")
+            students.pop(student_name)
+            print(f'Has eliminado a "{student_name}" de la lista')
+        # MUESTRA los estudiantes aprobados.
+        if question == "mostrar":
+            for key,value in students.items():
+                if value >= 6:
+                    print(f"{key}, nota media de: {value}")
+        # BUSCA al estudiante que el usuario escriba y devuelve su nombre con su respectivo promedio.
+        if question == "buscar":
+            student_name = input("¿Como se llama el estudiante?: ")
+            if student_name in students.keys():
+                print(f"{student_name}, nota media de: {students.get(student_name)}")
+            else:
+                print("Ese estudiante no existe.")
+        # Muestra TODOS los estudiantes.
+        if question == "todos":
+            print(students)
+        # SALE del programa.
+        if question == "salir":
+            continue_program = False
+    else:
+        print("Inserte un valor válido (agregar/quitar/mostrar/buscar/todos/salir)")
